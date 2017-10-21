703f0f33671c91793100d41a65ed116d4df5e4c2
Bastián Santana <bastian.santana@usach.cl>
2017-10-21 01:38:41 -0300

    CrossOrigin added

--- src/main/java/edu/usach/tbdgrupo5/rest/ArtistaService.java
+++ src/main/java/edu/usach/tbdgrupo5/rest/ArtistaService.java
@@ -2,6 +2,7 @@ package edu.usach.tbdgrupo5.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.usach.tbdgrupo5.entities.Artista;
 import edu.usach.tbdgrupo5.repository.ArtistaRepository;
 
+@CrossOrigin(origins = "http://localhost:8085")
 @RestController  
 @RequestMapping("/artistas")
 public class ArtistaService {
